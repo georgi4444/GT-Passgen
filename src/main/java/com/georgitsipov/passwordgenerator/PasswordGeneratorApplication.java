207519d499f6f3c41976e787c9b4dca4a183d6b4
ge64nub <ge64nub@mytum.de>
2021-09-09 23:02:41 +0200

    project setup

--- src/main/java/com/georgitsipov/passwordgenerator/PasswordGeneratorApplication.java
+++ src/main/java/com/georgitsipov/passwordgenerator/PasswordGeneratorApplication.java
@@ -1,13 +1,17 @@
 package com.georgitsipov.passwordgenerator;
 
+import com.vaadin.flow.component.dependency.NpmPackage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.vaadin.artur.helpers.LaunchUtil;
 
 @SpringBootApplication
+@NpmPackage(value = "lumo-css-framework", version = "^4.0.10")
+@NpmPackage(value = "line-awesome", version = "1.3.0")
 public class PasswordGeneratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PasswordGeneratorApplication.class, args);
+		LaunchUtil.launchBrowserInDevelopmentMode(SpringApplication.run(PasswordGeneratorApplication.class, args));
 	}
 
 }
